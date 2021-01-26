a366ec30195a16231cf5ad71bc112c24ee94ef56
ocawthorne <ocawthorne@gmail.com>
2021-01-26 19:06:49 +0000

    Build Concurrency Table

--- index.js
+++ index.js
@@ -1,43 +1,89 @@
-// document.getElementById('new-defendant').addEventListener('click', function() {
-//    let x = document.getElementById('new-defendant-form-container')
-//    if (x.style.display === "none") {
-//       x.style.display = "block"
-//    } else {
-//       x.style.display = "none"
-//    }
-// })
-
 function newDefendant() {
    let x = document.getElementById('new-defendant-form-container')
    if (x.style.display === "none") {
       x.style.display = "block"
+      return true
    } else {
       x.style.display = "none"
+      return false
    }
 }
 
-document.getElementById('new-count').addEventListener('click', function(e) { // Inserting new counts in New Defendant
+document.getElementsByClassName('new-count')[0].addEventListener('click', function(e) { // Inserting new counts in New Defendant
+   // Adding offence, Y/M/D fields
    e.preventDefault()
-
    let countNum = document.querySelectorAll('.count').length
-   nodes = document.querySelectorAll('.count')
-   el = document.createElement("div")
+   let nodes = document.querySelectorAll('.count')
+   let el = document.createElement("div")
+
    el.innerHTML = `
    <label for="count" class="count">Count ${countNum+1}:</label>
    <input type="text" name="count_${countNum+1}" placeholder="offence" value required>
-   <input type="number" name="years_${countNum+1}" placeholder="years" class="num-field years">
-   <input type="number" name="months_${countNum+1}" placeholder="months" class="num-field months">
-   <input type="number" name="days_${countNum+1}" placeholder="days" class="num-field days"><br><br> 
-   ` // FIXME Needs updating to match index.html
+   <input type="number" min="0" name="years_${countNum+1}" placeholder="years" class="num-field years" onkeyup="calculateSentence()">
+   <input type="number" min="0" name="months_${countNum+1}" placeholder="months" class="num-field months" onkeyup="calculateSentence()">
+   <input type="number" min="0" name="days_${countNum+1}" placeholder="days" class="num-field days" onkeyup="calculateSentence()"><br><br> 
+   `
+   //TODO needs a delete button for superfluous counts
    nodes[nodes.length-1].parentNode.insertBefore(el, nodes.nextSibling)
+
+   // Adding rows and columns to Concurrency Table
+   let table = document.querySelector('#con-tab').getElementsByTagName('tbody')[0]
+   // Add new row
+   row = document.createElement('tr')
+   row.id = `row${countNum+1}`
+   row.innerHTML = `<th>${countNum+1}</th>`+`<td>-</td>`.repeat(countNum+1)
+   // if (countNum > 0) {
+      for (let i=1; i<countNum+1; i++) {
+         prevRow = document.getElementById(`row${i}`)
+         prevRow.innerHTML += `<td><input type="checkbox" class="row-${i} col-${i+1}" onclick="calculateSentence()"></td>`
+      }
+   // }
+   table.appendChild(row)
+
+   // Add new column
+   header = document.createElement('th')
+   let text = `<th>${countNum+1}</th>`
+   header.innerHTML = text
+   table.firstElementChild.appendChild(header)
+
+   document.getElementById('concurrent-question').style.display = "block"
+
+})
+
+document.getElementById('concurrent-check').addEventListener('click', function(e) {
+   let x = document.getElementById('concurrency-table')
+   if (e.target.checked || document.querySelectorAll('.count').length < 3) {
+      x.style.display = "none"
+   } else {
+      x.style.display = "block"
+   }
 })
 
 function calculateSentence() {
-   let years = 0
-   Object.entries(document.getElementsByClassName('years')).forEach(function(el) {
-      years += parseInt(el[1].value)
+   let years = months = days = 0
+   // Object.entries(document.getElementsByClassName('years')).forEach(function(el) {
+   document.querySelectorAll('.years,.months,.days').forEach(function(el) {
+      let val = parseInt(el.value)
+      switch(el.placeholder) {
+         case "years":
+            if (!isNaN(val)) years += val
+            break
+         case "months":
+            if (!isNaN(val)) months += val
+            break
+         case "days":
+            if (!isNaN(val)) days += val
+      }
    })
-   sum
+   document.getElementById('sentence-comp').innerText = convertToMD(years, months, days)
+}
+
+function convertToMD(years, months, days) { // Based on 30-day months
+   daysDisp = days % 30
+   monthR = Math.floor(days / 30)
+   monthsDisp = (months + monthR + years*12)
+
+   return `${monthsDisp} months, ${daysDisp} days`
 }
 
 function save() {
